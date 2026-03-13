08c11f562a2811e444605934bef8afcc5457c22e
eskalaiyarasan <eskalaiyarasan@gmail.com>
2026-03-13 18:05:16 +0530

    Add engine configuration data classes

---
+++ config/interface/engine_config_database.py
@@ -0,0 +1,54 @@
+from dataclasses import dataclass
+
+module_name="rule"
+
+@dataclass(frozen=True)
+class TimePerMove:
+    max: int
+    enabled: bool 
+
+
+@dataclass(frozen=True)
+class ClockRule:
+    enabled: bool
+    min: int
+
+
+@dataclass(frozen=True)
+class Fruiting:
+    dormant: bool
+    period: int
+
+@dataclass(frozen=True)
+class BoardConfig:
+    nseeds: int
+    npits: int
+    nside: int
+
+@dataclass(frozen=True)
+class VarRuleConfig:
+    board: BoardConfig
+    fruit: Fruiting
+    time: TimePerMove
+    clock: ClockRule
+    kingzpit: bool
+    capture: str
+    captureplus: bool 
+
+
+@dataclass(frozen=True)
+class FixedRuleConfig:
+    level: str
+    algo: str 
+    engine:str 
+
+
+@dataclass(frozen=True)
+class ruleConfig:
+    var: VarRuleConfig
+    fixed: FixedRuleConfig
+    yaml: str
+
+@dataclass(frozen=True)
+class PitpalRuleConfig:
+    rule: RuleConfig
